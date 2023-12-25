255ede95e940a322369d5dfd025f81f1d666c6e9
Shamil Magomedov <shomainkognito@yandex.ru>
2023-12-25 12:46:23 +0300

    #fix vue config file

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,5 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  transpileDependencies: true,
-  publicPath: '/gitogram/dist/'
+  transpileDependencies: true
+  // publicPath: '/gitogram/dist/'
 })
